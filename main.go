25ed9bb9ba0b7b2b4bd4c6f8588a83bacfede719
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-02-11 16:53:15 +0000

    Cassidy third commit

--- main.go
+++ main.go
@@ -6,12 +6,33 @@ package main
 // https://pkg.go.dev/encoding/json#Unmarshal  -- used for the data
 // https://groupietrackers.herokuapp.com/api -- JSON data for website
 // https://github.com/Muslimah94/groupie-tracker -- code example on github
+// https://gist.github.com/rodkranz/0a8ed14fa44b5860f6668efae02b3ea5 -- big files using marshal
 
 import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
+// type artistData struct {
+// 	ID           int                 `json:"id"`
+// 	Image        string              `json:"image"`
+// 	Name         string              `json:"name"`
+// 	Members      []string            `json:"members"`
+// 	CreationDate int                 `json:"creationDate"`
+// 	FirstAlbum   string              `json:"firstAlbum"`
+// 	Relation     string              `json:"relations"`
+// 	Concerts     map[string][]string `json:"datesLocations"`
+// }
+
+// type relation struct {
+// 	ID       int                 `json:"id"`
+// 	Concerts map[string][]string `json:"datesLocations"`
+// }
+
+// func (e *artistData) Unmarshal(b []byte) error {
+// 	return json.Unmarshal(b, e)
+// }
 
 func main() {
 	http.HandleFunc("/", process)
@@ -20,9 +41,26 @@ func main() {
 	fmt.Printf("Starting server at port localhost:8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
-	var jsonGroupie = []byte('[
-		
-	]')
+	// type Groupie struct {
+	// 	artists   string
+	// 	locations string
+	// 	dates     string
+	// 	relation  string
+	// }
+
+	// var groupies []interface{}
+	// err := json.Unmarshal(str, &groupies)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// for _, v := range groupies {
+	// 	z := v.(map[string]interface{})
+	// 	for k2, v2 := range z {
+	// 		fmt.Println("Key:", k2, "Value:", v2)
+	// 	}
+	// }
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
@@ -41,19 +79,17 @@ func process(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		input := r.FormValue("input")
-		banner := r.FormValue("Banner")
+		// input := r.FormValue("input")
+		// banner := r.FormValue("Banner")
 
-		response, err := AsciiArt(input, banner)
-		if err != nil {
-			http.Error(w, "No such file or directory: Internal Server Error 500", http.StatusInternalServerError)
-			return
-		}
+		// response, err := AsciiArt(input, banner)
+		// if err != nil {
+		// 	http.Error(w, "No such file or directory: Internal Server Error 500", http.StatusInternalServerError)
+		// 	return
+		// }
 
-		_, _ = w.Write([]byte(response)) // Write returns the response with a 200 status code in the header as this is built into the Write function
+		// _, _ = w.Write([]byte(response)) // Write returns the response with a 200 status code in the header as this is built into the Write function
 	default:
 		http.Error(w, "Sorry, only GET and POST methods are supported.", http.StatusUnsupportedMediaType)
 	}
 }
-
-
