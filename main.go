3144dfafc6412d4b8dd077631f3ddfd314036033
jagodasad <jago.sadowska@gmail.com>
2022-02-15 09:25:01 +0000

    worked on server and html

--- main.go
+++ main.go
@@ -1,95 +1,487 @@
 package main
 
-// go code for Rick and Morty API example
-// https://github.com/pitakill/rickandmortyapigowrapper
-// https://git.learn.01founders.co/root/public/src/branch/master/subjects/groupie-tracker/audit
-// https://pkg.go.dev/encoding/json#Unmarshal  -- used for the data
-// https://groupietrackers.herokuapp.com/api -- JSON data for website
-// https://github.com/Muslimah94/groupie-tracker -- code example on github
-// https://gist.github.com/rodkranz/0a8ed14fa44b5860f6668efae02b3ea5 -- big files using marshal
-
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
-// type artistData struct {
-// 	ID           int                 `json:"id"`
-// 	Image        string              `json:"image"`
-// 	Name         string              `json:"name"`
-// 	Members      []string            `json:"members"`
-// 	CreationDate int                 `json:"creationDate"`
-// 	FirstAlbum   string              `json:"firstAlbum"`
-// 	Relation     string              `json:"relations"`
-// 	Concerts     map[string][]string `json:"datesLocations"`
-// }
+const baseURL = "https://groupietrackers.herokuapp.com/api"
 
-// type relation struct {
-// 	ID       int                 `json:"id"`
-// 	Concerts map[string][]string `json:"datesLocations"`
-// }
-
-// func (e *artistData) Unmarshal(b []byte) error {
-// 	return json.Unmarshal(b, e)
-// }
-
-func main() {
-	http.HandleFunc("/", process)
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-
-	fmt.Printf("Starting server at port localhost:8080\n")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// type Groupie struct {
-	// 	artists   string
-	// 	locations string
-	// 	dates     string
-	// 	relation  string
-	// }
-
-	// var groupies []interface{}
-	// err := json.Unmarshal(str, &groupies)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// for _, v := range groupies {
-	// 	z := v.(map[string]interface{})
-	// 	for k2, v2 := range z {
-	// 		fmt.Println("Key:", k2, "Value:", v2)
-	// 	}
-	// }
+type MyArtistFull struct {
+	ID             int                 `json:"id"`
+	Image          string              `json:"image"`
+	Name           string              `json:"name"`
+	Members        []string            `json:"members"`
+	CreationDate   int                 `json:"creationDate"`
+	FirstAlbum     string              `json:"firstAlbum"`
+	Locations      []string            `json:"locations"`
+	ConcertDates   []string            `json:"concertDates"`
+	DatesLocations map[string][]string `json:"datesLocations"`
 }
 
-func process(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "404 Status not found", http.StatusNotFound)
+type MyArtist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type MyLocation struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
+}
+
+type MyRelation struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+type MyDate struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type MyDates struct {
+	Index []MyDate `json:"index"`
+}
+
+type MyLocations struct {
+	Index []MyLocation `json:"index"`
+}
+type MyRelations struct {
+	Index []MyRelation `json:"index"`
+}
+
+var ArtistsFull []MyArtistFull
+var Artists []MyArtist
+var Dates MyDates
+var Locations MyLocations
+var Relations MyRelations
+
+func GetArtistsData() error {
+	resp, err := http.Get(baseURL + "/artists")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &Artists)
+	return nil
+}
+
+func GetDatesData() error {
+	resp, err := http.Get(baseURL + "/dates")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &Dates)
+	return nil
+}
+
+func GetLocationsData() error {
+	resp, err := http.Get(baseURL + "/locations")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &Locations)
+	return nil
+}
+
+func GetRelationsData() error {
+	resp, err := http.Get(baseURL + "/relation")
+	if err != nil {
+		return errors.New("Error by get")
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.New("Error by ReadAll")
+	}
+	json.Unmarshal(bytes, &Relations)
+	return nil
+}
+
+func GetData() error {
+	if len(ArtistsFull) != 0 {
+		return nil
+	}
+	err1 := GetArtistsData()
+	err2 := GetLocationsData()
+	err3 := GetDatesData()
+	err4 := GetRelationsData()
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+		return errors.New("Error by get data artists, locations, dates")
+	}
+	for i := range Artists {
+		var tmpl MyArtistFull
+		tmpl.ID = i + 1
+		tmpl.Image = Artists[i].Image
+		tmpl.Name = Artists[i].Name
+		tmpl.Members = Artists[i].Members
+		tmpl.CreationDate = Artists[i].CreationDate
+		tmpl.FirstAlbum = Artists[i].FirstAlbum
+		tmpl.Locations = Locations.Index[i].Locations
+		tmpl.ConcertDates = Dates.Index[i].Dates
+		tmpl.DatesLocations = Relations.Index[i].DatesLocations
+		ArtistsFull = append(ArtistsFull, tmpl)
+	}
+	return nil
+}
+
+func GetArtistByID(id int) (MyArtist, error) {
+	for _, artist := range Artists {
+		if artist.ID == id {
+			return artist, nil
+		}
+	}
+	return MyArtist{}, errors.New("Not found")
+}
+
+func GetDateByID(id int) (MyDate, error) {
+	for _, date := range Dates.Index {
+		if date.ID == id {
+			return date, nil
+		}
+	}
+	return MyDate{}, errors.New("Not found")
+}
+
+func GetLocationByID(id int) (MyLocation, error) {
+	for _, location := range Locations.Index {
+		if location.ID == id {
+			return location, nil
+		}
+	}
+	return MyLocation{}, errors.New("Not found")
+}
+
+func GetRelationByID(id int) (MyRelation, error) {
+	for _, relation := range Relations.Index {
+		if relation.ID == id {
+			return relation, nil
+		}
+	}
+	return MyRelation{}, errors.New("Not found")
+}
+
+func GetFullDataById(id int) (MyArtistFull, error) {
+	for _, artist := range ArtistsFull {
+		if artist.ID == id {
+			return artist, nil
+		}
+	}
+	return MyArtistFull{}, errors.New("Not found")
+}
+
+var data []MyArtistFull
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	err := GetData()
+	if err != nil {
+		errors.New("Error by get data")
+	}
+
+	main := r.FormValue("main")
+	search := r.FormValue("search")
+	filterByCreationFrom := r.FormValue("startCD")
+	filterByCreationTill := r.FormValue("endCD")
+	location := r.FormValue("location-filter")
+	var mem1, mem2, mem3, mem4, mem5, mem6, mem7, mem8 int
+	mem1, err1 := strconv.Atoi(r.FormValue("mem1"))
+	if err1 != nil {
+		mem1 = 0
+	}
+	mem2, err2 := strconv.Atoi(r.FormValue("mem2"))
+	if err2 != nil {
+		mem2 = 0
+	}
+	mem3, err3 := strconv.Atoi(r.FormValue("mem3"))
+	if err3 != nil {
+		mem3 = 0
+	}
+	mem4, err4 := strconv.Atoi(r.FormValue("mem4"))
+	if err4 != nil {
+		mem4 = 0
+	}
+	mem5, err5 := strconv.Atoi(r.FormValue("mem5"))
+	if err5 != nil {
+		mem5 = 0
+	}
+	mem6, err6 := strconv.Atoi(r.FormValue("mem6"))
+	if err6 != nil {
+		mem6 = 0
+	}
+	mem7, err7 := strconv.Atoi(r.FormValue("mem7"))
+	if err7 != nil {
+		mem7 = 0
+	}
+	mem8, err8 := strconv.Atoi(r.FormValue("mem8"))
+	if err8 != nil {
+		mem8 = 0
+	}
+	mem := []int{mem1, mem2, mem3, mem4, mem5, mem6, mem7, mem8}
+	sum := 0
+	for _, n := range mem {
+		sum += n
+	}
+	println("startCD:", filterByCreationFrom)
+	fmt.Println("mem:", mem)
+
+	filterByFA := r.FormValue("startFA")
+	filterByFAend := r.FormValue("endFA")
+	fmt.Println("filterFA:", filterByFA)
+	fmt.Println("filterFAend:", filterByFAend)
+
+	if !(search == "" && len(data) != 0) {
+		data = Search(search)
+	}
+
+	if filterByCreationFrom != "" || filterByCreationTill != "" {
+		if filterByCreationFrom == "" {
+			filterByCreationFrom = "1900"
+		}
+		if filterByCreationTill == "" {
+			filterByCreationTill = "2020"
+		}
+		data = FilterByCreation(data, filterByCreationFrom, filterByCreationTill)
+	}
+
+	if filterByFA != "" || filterByFAend != "" {
+		if filterByFA == "" {
+			filterByFA = "1900-01-01"
+		}
+		if filterByFAend == "" {
+			filterByFAend = "2020-03-03"
+		}
+		data = FilterByAlbumDate(data, filterByFA, filterByFAend)
+	}
+
+	if sum != 0 {
+		data = FilterByMember(data, mem)
+	}
+
+	if location != "" {
+		data = FilterByLocation(data, location)
+	}
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
 		return
 	}
 
-	switch r.Method {
-	case "GET":
+	if main == "Main Page" {
+		data = Search("a")
+	}
 
-		http.ServeFile(w, r, "index.html")
-	case "POST":
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest) // if there is an error it returns bad request 400
-			return
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
+
+func concertPage(w http.ResponseWriter, r *http.Request) {
+
+	idStr := r.FormValue("concert")
+	id, _ := strconv.Atoi(idStr)
+	artist, _ := GetFullDataById(id)
+
+	for key, value := range artist.DatesLocations {
+		fmt.Print(key + "  - ")
+		for _, e := range value {
+			println(e)
+		}
+	}
+
+	tmpl, err := template.ParseFiles("concert.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	if err := tmpl.Execute(w, artist); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+}
+
+func ConverterStructToString() ([]string, error) {
+	var data []string
+	for i := 1; i <= len(Artists); i++ {
+		artist, err1 := GetArtistByID(i)
+		locations, err2 := GetLocationByID(i)
+		date, err3 := GetDateByID(i)
+		if err1 != nil || err2 != nil || err3 != nil {
+			return data, errors.New("Error by converter")
 		}
 
-		// input := r.FormValue("input")
-		// banner := r.FormValue("Banner")
-
-		// response, err := AsciiArt(input, banner)
-		// if err != nil {
-		// 	http.Error(w, "No such file or directory: Internal Server Error 500", http.StatusInternalServerError)
-		// 	return
-		// }
-
-		// _, _ = w.Write([]byte(response)) // Write returns the response with a 200 status code in the header as this is built into the Write function
-	default:
-		http.Error(w, "Sorry, only GET and POST methods are supported.", http.StatusUnsupportedMediaType)
+		str := artist.Name + " "
+		for _, member := range artist.Members {
+			str += member + " "
+		}
+		str += strconv.Itoa(artist.CreationDate) + " "
+		str += artist.FirstAlbum + " "
+		for _, location := range locations.Locations {
+			str += location + " "
+		}
+		for _, d := range date.Dates {
+			str += d + " "
+		}
+		data = append(data, str)
 	}
+	println("Convert to str Done!")
+	return data, nil
+}
+
+func Search(search string) []MyArtistFull {
+	if search == "" {
+		return ArtistsFull
+	}
+	art, err := ConverterStructToString()
+	if err != nil {
+		errors.New("Error by converter")
+	}
+	var search_artist []MyArtistFull
+
+	for i, artist := range art {
+		lower_band := strings.ToLower(artist)
+		for i_name, l_name := range []byte(lower_band) {
+			lower_search := strings.ToLower(search)
+			if lower_search[0] == l_name {
+				lenght_name := 0
+				indx := i_name
+				for _, l := range []byte(lower_search) {
+					if l == lower_band[indx] {
+						if indx+1 == len(lower_band) {
+							break
+						}
+						indx++
+						lenght_name++
+					} else {
+						break
+					}
+				}
+				if len(search) == lenght_name {
+					band, _ := GetFullDataById(i + 1)
+					search_artist = append(search_artist, band)
+					break
+				}
+			}
+		}
+
+	}
+	println("Search str Done!")
+	return search_artist
+}
+
+func FilterByCreation(data []MyArtistFull, from string, till string) []MyArtistFull {
+	if from == "" || till == "" {
+		return data
+	}
+	fromInt, err1 := strconv.Atoi(from)
+	tillInt, err2 := strconv.Atoi(till)
+	if err1 != nil || err2 != nil {
+		errors.New("Error by filter by creation date data")
+	}
+
+	var search_artist []MyArtistFull
+
+	for _, artist := range data {
+		if artist.CreationDate >= fromInt && artist.CreationDate <= tillInt {
+			search_artist = append(search_artist, artist)
+		}
+	}
+	return search_artist
+}
+
+func FilterByMember(data []MyArtistFull, mem []int) []MyArtistFull {
+	var tmp []MyArtistFull
+	for _, bandmem := range data {
+		for _, num := range mem {
+			if len(bandmem.Members) == num {
+				tmp = append(tmp, bandmem)
+			}
+
+		}
+	}
+	return tmp
+}
+
+func FilterByAlbumDate(data []MyArtistFull, albumStart string, albumEnd string) []MyArtistFull {
+	layOut := "2006-01-02"
+
+	dateStart, err1 := time.Parse(layOut, albumStart)
+	dateEnd, err2 := time.Parse(layOut, albumEnd)
+	if err1 != nil || err2 != nil {
+		errors.New("Error by First Album date convert for filter")
+	}
+	var tmp []MyArtistFull
+
+	layOutData := "02-01-2006"
+	for _, band := range data {
+		date, err := time.Parse(layOutData, band.FirstAlbum)
+		if err != nil {
+			errors.New("Error by First Album date convert for filter")
+		}
+		if dateStart.Before(date) && dateEnd.After(date) {
+			tmp = append(tmp, band)
+		}
+	}
+	return tmp
+}
+
+func FilterByLocation(data []MyArtistFull, location string) []MyArtistFull {
+	var tmp []MyArtistFull
+
+	for _, band := range data {
+		for _, loc := range band.Locations {
+			if location == loc {
+				tmp = append(tmp, band)
+			}
+		}
+	}
+	return tmp
+}
+
+func main() {
+
+	// static folder
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/concert", concertPage)
+
+	port := ":8080"
+	println("Server listen on port:", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal("Listen and Server", err)
+	}
+
 }
