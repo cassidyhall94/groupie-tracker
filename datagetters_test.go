7398e2dfb886227a06ae4ead501791d9155e5f9d
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2022-02-24 10:37:50 +0000

    test file2

--- datagetters_test.go
+++ datagetters_test.go
@@ -3,4 +3,37 @@ package main
 import "testing"
 
 func TestGetArtistsData(t *testing.T) {
+
+	artists, err := GetArtistsData()
+	if err != nil {
+		t.Fatalf("artists err:%+v", err)
+	}
+	if len(artists) < 1 {
+		t.Fatalf("artists failed due to length < 1")
+	}
+}
+
+func TestGetArtistByID(t *testing.T) {
+	testCases := []struct{
+		input  struct{
+			ID int
+			MyArtists []MyArtist
+		}
+		want 	MyArtist
+		wantErr bool
+	}{
+		{
+			input: {
+				ID: 0,
+				MyArtists: {
+					{},
+				},
+			},
+			want: {
+				ID: ,
+				Image: ,
+			},
+			wantErr: ,
+		},
+	}
 }
